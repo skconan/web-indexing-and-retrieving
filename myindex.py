8e66618543b41818b614c87a235fd77eb28b516f
skconan <supakit.kr@gmail.com>
2018-09-21 14:43:34 +0700

    create index

--- myindex.py
+++ myindex.py
@@ -4,7 +4,7 @@ import constants as CONST
 from elasticsearch import Elasticsearch
 import sys
 import json
-
+import time
 
 def parse_html(file):
     f = gzip.open(file)
@@ -25,7 +25,7 @@ def parse_html(file):
 def index_doc(doc):
     myid = doc['Url']
     res = es.index(index=index, doc_type=doctype, id=myid, body=doc)
-
+    return res
 
 # def parse_html_2(file):
 #     res = []
@@ -38,10 +38,25 @@ def index_doc(doc):
 
 # parse_html_2(CONST.PATH_PROJECT+'\html\html')
 
+start_time = time.time()
+
 file = CONST.PATH_PROJECT+'/webpage.txt.gz'
 json_docs = parse_html(file)
 es_host = "http://localhost:9200"
 index = "ku"
 doctype = "webpage"
 es = Elasticsearch(es_host)
-es.index(index=index, doc_type=doctype, id=json_docs[0]['Url'], body=json_docs[0])
+# es.index(index=index, doc_type=doctype, id=json_docs[0]['Url'], body=json_docs[0])
+
+
+number_of_doc = 0
+for i in json_docs:
+    i['Links'] = ''
+    index_doc(i)
+    number_of_doc += 1
+    if number_of_doc % 100 == 0:
+        print('.',end='')
+print('\n', number_of_doc, 'doc(s) indexed')
+
+end_time = time.time()
+print(end_time - start_time)
